4dff97661bbcdd503806e1831cc336d6a3b6dc62
Akankshya Pany <78838119+AkankshyaPany@users.noreply.github.com>
2023-08-04 13:49:43 +0530

    Add files via upload

---
+++ Myjava/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String name1 ="akk";
+        String name3 ="akwk";
+        String name2 =name1+name3;
+        System.out.println(name2);
+    }
+}
